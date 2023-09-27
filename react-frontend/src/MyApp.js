c6a7b9841113b1600922fbdff48217ae2b361283
Brandon Eng <ebdon101@gmail.com>
2023-09-27 09:57:49 -0700

    initialize state

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,31 +1,27 @@
 // src/MyApp.js
-import React from "react";
-import Table from "./Table";
+import React, {useState} from 'react';
+import Table from './Table';
 
-const characters = [
-    {
-      name: "Banana",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ];
-  
-  function MyApp() {
-    return (
-        <div className="container">
-        <Table characterData={characters} />
-      </div>
-    );
-  }
+function MyApp() {
+  const [characters, setCharacters] = useState([
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      // the rest of the data
+    ]); 
+    function removeOneCharacter (index) {
+	    const updated = characters.filter((character, i) => {
+	        return i !== index
+	    });
+	  setCharacters(updated);
+	}
+    
+  return (
+    <div className="container">
+        <Table characterData={characters} 
+	        removeCharacter={removeOneCharacter} />
+    </div>  
+  ) 
+}
 export default MyApp;
